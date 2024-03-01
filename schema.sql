806b25fc79187f2ee4192add68493d784f107f02
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2024-02-29 18:32:50 -0600

    database changed

--- schema.sql
+++ schema.sql
@@ -1,6 +1,10 @@
-CREATE TABLE IF NOT EXISTS blog_posts (
+CREATE TABLE IF NOT EXISTS blog_crk_posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
+    cookie_name VARCHAR(255),
+    battle_role VARCHAR(255),
+    ability TEXT NOT NULL,
     content TEXT NOT NULL,
+    skins TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
