fef3f662dc8a5971af7c2b15651f1327ee7b2e53
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2024-02-29 18:31:31 -0600

    js and json done

---
+++ conn.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise'
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'blog_user',
+    database: 'blog_db',
+    password: 'blog_password',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+export default pool
---
+++ db.js
@@ -0,0 +1,69 @@
+import conn from './conn.js'
+
+export async function getAllPosts() {
+ const [rows] = await conn.query('SELECT * FROM blog_posts')
+ return rows
+}
+
+export async function createPost(
+    title, 
+    cookieName,
+    battle_role,
+    abilityDesc,
+    content,
+    skins) {
+    const [result] = await conn.query('INSERT INTO blog_crk_posts (title, cookie_name, battle_role, ability, content, skins) VALUES (?, ?, ?, ?, ?, ?)', 
+    [
+        title, 
+        cookieName,
+        battle_role,
+        abilityDesc,
+        content,
+        skins
+    ])
+    const rowInserted = await getPostByID(result.insertId)
+    return result
+}
+
+export async function getPostByID(blogPostID) {
+    const [rows] = conn.query('SELECT * FROM blog_crk_posts WHERE id = ?',
+    [blogPostID])
+    return rows
+}
+
+export async function deletePost(blogPostID) {
+    try {
+        const [result] = await conn.query('DELETE FROM blog_crk_posts WHERE id = ?', [blogPostID]);
+        
+        if (result.affectedRows === 1) {
+          console.log(`El Post con ID ${blogPostID} fue eliminada exitosamente.`);
+          return true
+        } else {
+          console.error(`No se encontró ningun post con el ID ${blogPostID} para eliminar.`);
+          return false; // Indica que no se encontró la fila para eliminar
+        }
+    } 
+    catch (error) {
+        console.error('Error al intentar eliminar el post:', error);
+        return false; // Indica que ocurrió un error al intentar eliminar la fila
+    }
+}
+
+export async function updateSkinInPost(blogPostID, skins) {
+    try {
+        const [result] = await conn.query('UPDATE blog_crk_posts SET skins = ? WHERE id = ?', [skins, blogPostID]);
+        
+        if (result.affectedRows === 1) {
+          console.log(`La información del post con ID ${blogPostID} fue actualizada exitosamente.`);
+          const updatedPost = await getPostByID(blogPostID);
+          return updatedPost;
+        } else {
+          console.error(`No se encontró ningun post con el ID ${blogPostID} para actualizar.`);
+          return null; // Indica que no se encontró el post para actualizar
+        }
+    } 
+    catch (error) {
+        console.error('Error al intentar actualizar la informacion del post:', error);
+        return null; // Indica que ocurrió un error al intentar actualizar la información del post
+    }
+}
